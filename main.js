c751be0d8a528da9ddbc0daf7dec7e0faef95d73
Ann Wangui Wairimu <ann99wangui@gmail.com>
2023-03-31 20:39:45 +0300

    completed

--- main.js
+++ main.js
@@ -2,9 +2,34 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+const errorModal = document.querySelector('#modal');
+errorModal.classList.add('hidden');
 
+const emptyHeart = document.querySelector('.like-glyph');
+emptyHeart.addEventListener('click', function() {
+  mimicServerCall()
+    .then(function(response) {
+      const fullHeart = emptyHeart.querySelector('.like-glyph-icon');
+      fullHeart.textContent = FULL_HEART;
+      emptyHeart.classList.add('activated-heart');
+      emptyHeart.classList.remove('like-glyph');
+    })
+    .catch(function(error) {
+      const errorMessage = errorModal.querySelector('#modal-message');
+      errorMessage.textContent = error;
+      errorModal.classList.remove('hidden');
+      setTimeout(function() {
+        errorModal.classList.add('hidden');
+      }, 3000);
+    });
+});
 
+const fullHeart = document.querySelector('.activated-heart');
+fullHeart.addEventListener('click', function() {
+  fullHeart.textContent = EMPTY_HEART;
+  emptyHeart.classList.add('like-glyph');
+  emptyHeart.classList.remove('activated-heart');
+});
 
 
 //------------------------------------------------------------------------------
